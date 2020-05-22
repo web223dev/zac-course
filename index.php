05f7ced09fe0102a6de342703a05152d84b9cfa6
Dmitrii <ruslanus866@outlook.com>
2020-05-22 18:46:31 +0800

    Working with the Loop

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <?php get_header('splash'); ?>
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <?php if (have_posts()) : while (have_posts()) : the_post() ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
 
+                        <?php the_title('<h1>', '</h1>') ?>
+
+                    </header>
+
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+            <?php endwhile;
+        else : ?>
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 <header class="entry-header">
-                    
-                    <h1>index.php</h1>
+
+                    <h1><?php esc_html_e( '404', 'wphierarchy' ) ?></h1>
 
                 </header>
 
                 <div class="entry-content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p><?php esc_html_e( 'Sorry No content found', 'wphierarchy' ) ?></p>
                 </div>
             </article>
+        <?php endif; ?>
+    </main>
+</div>
 
-        </main>
-    </div>
-
-    <?php get_sidebar(); ?>
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
