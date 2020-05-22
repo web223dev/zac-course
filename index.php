458cb674c55ba4398c0bdfe1dec3eba268faf606
Dmitrii <ruslanus866@outlook.com>
2020-05-22 22:22:57 +0800

    Creating Content Includes

--- index.php
+++ index.php
@@ -3,30 +3,9 @@
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
         <?php if (have_posts()) : while (have_posts()) : the_post() ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
-
-                        <?php the_title('<h1>', '</h1>') ?>
-
-                    </header>
-
-                    <div class="entry-content">
-                        <?php the_content(); ?>
-                    </div>
-                </article>
-            <?php endwhile;
-        else : ?>
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
-
-                    <h1><?php esc_html_e( '404', 'wphierarchy' ) ?></h1>
-
-                </header>
-
-                <div class="entry-content">
-                    <p><?php esc_html_e( 'Sorry No content found', 'wphierarchy' ) ?></p>
-                </div>
-            </article>
+            <?php get_template_part('template-parts/content') ?>
+            <?php endwhile; else : ?>
+            <?php get_template_part('template-parts/content', 'none') ?>
         <?php endif; ?>
     </main>
 </div>
