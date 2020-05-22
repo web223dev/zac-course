02379692c0b736d7dd8d3d724f4e459662e56739
Dmitrii <ruslanus866@outlook.com>
2020-05-22 16:13:23 +0800

    Add Markup to the Theme part 2

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php get_header('splash'); ?>
-<h1>index.php</h1>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    
+                    <h1>index.php</h1>
+
+                </header>
+
+                <div class="entry-content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </article>
+
+        </main>
+    </div>
+
 <?php get_footer(); ?>
