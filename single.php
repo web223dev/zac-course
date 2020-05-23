e71920ca07638b3674a5c9cf76a43735723586f8
Dmitrii <ruslanus866@outlook.com>
2020-05-23 11:48:22 +0800

    Working with the single.php template

--- single.php
+++ single.php
@@ -3,11 +3,11 @@
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
         <?php if (have_posts()) : while (have_posts()) : the_post() ?>
-            <?php get_template_part('template-parts/content', 'page') ?>
+            <?php get_template_part('template-parts/content') ?>
             <?php endwhile; else : ?>
             <?php get_template_part('template-parts/content', 'none') ?>
         <?php endif; ?>
-        <p>Template: Singular.php</p>
+        <p>Template: single.php</p>
     </main>
 </div>
 
