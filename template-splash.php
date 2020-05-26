52be036be6a2762e1ec034a6d56d250485bc9354
Dmitrii <ruslanus866@outlook.com>
2020-05-26 16:06:19 +0800

    Working with Custom Templates

---
+++ template-splash.php
@@ -0,0 +1,18 @@
+<?php  
+    // Template Name: Splash Page
+    // Template Post Type: post, page, custom-post-type
+?>
+<?php get_header('splash'); ?>
+
+<div id="primary" class="content-area extended">
+    <main id="main" class="site-main" role="main">
+        <?php if (have_posts()) : while (have_posts()) : the_post() ?>
+            <?php get_template_part('template-parts/content', 'page') ?>
+            <?php endwhile; else : ?>
+            <?php get_template_part('template-parts/content', 'none') ?>
+        <?php endif; ?>
+        <p>Template: template-splash.php</p>
+    </main>
+</div>
+
+<?php get_footer(); ?>
