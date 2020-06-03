bbf1d3eaaaf03853de1d71aa1bdd4b5763094dd9
Dmitrii <ruslanus866@outlook.com>
2020-06-03 18:27:41 +0800

    Exploring Action Hooks with wp_actions and wp_filter

--- front-page.php
+++ front-page.php
@@ -1,5 +1,10 @@
 <?php get_header('splash'); ?>
-
+<?php
+    // echo '<pre>';
+    // print_r($wp_actions);
+    // print_r($wp_filter['wp_enqueue_scripts']);
+    // echo '</pre>';
+?>
 <div id="secondary" class="content-area extended">
     <main id="main" class="site-main" role="main">
         <?php if (have_posts()) : while (have_posts()) : the_post() ?>
