a15881f5dd619922dd5d57d48fa1a1cd1b80d9a3
Dmitrii <ruslanus866@outlook.com>
2020-06-04 17:50:54 +0800

    DEMO - WordPress Action Hook - save_post()

--- functions.php
+++ functions.php
@@ -110,7 +110,50 @@ function wphierarchy_members_logged_out_redirect(){
 
 add_action( 'template_redirect', 'wphierarchy_members_logged_out_redirect', 10);
 
+// Add 'draft' in the title when save post by draft
+function wphooks_add_draft_to_titles( $post_id, $post ) {
 
+    // If post revision do not proceed
+    if ( wp_is_post_revision( $post_id ) ) {
+      return;
+    }
+  
+  
+    // Add or remove "DRAFT: " from title depending on status
+    if( 'draft'   === $post->post_status &&
+        'DRAFT: ' !== substr( $post->post_title, 0, 7 ) ) {
+  
+        // Add 'DRAFT: ' to the title
+        $post->post_title = 'DRAFT: ' . $post->post_title;
+  
+    } elseif ( 'publish' === $post->post_status &&
+               'DRAFT: ' === substr( $post->post_title, 0, 7 ) ) {
+  
+        // Remove 'DRAFT: ' from the title
+        $post->post_title = substr( $post->post_title, 7 );
+  
+    }
+  
+  
+    // If slug starts with 'draft-' remove it
+    if( 'draft-' === substr( $post->post_name, 0, 6 ) ) {
+  
+        $post->post_name = substr( $post->post_name, 6 );
+  
+    }
+  
+    // Unhook wphooks_add_draft_to_titles so it doesn't loop infinitely
+    remove_action('save_post', 'wphooks_add_draft_to_titles');
+  
+    // Update the post
+    wp_update_post( $post );
+  
+    // Re-hook wphooks_add_draft_to_titles
+    add_action('save_post', 'wphooks_add_draft_to_titles');
+  
+  
+  }
+  add_action( 'save_post', 'wphooks_add_draft_to_titles' );
 
 
 
